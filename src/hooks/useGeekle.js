5d27796cfff08d70eaaba9dd366740c9df238e37
Drew <aamunat2@jh.edu>
2022-06-01 16:38:31 -0400

    set up format guess for green

--- src/hooks/useGeekle.js
+++ src/hooks/useGeekle.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 
 const useGeekle = (solution) => {
 
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState('');
-  const [guesses, setGuesses] = useState([]); // formatted guess
+  const [guesses, setGuesses] = useState([...Array(6)]); // formatted guess
   const [history, setHistory] = useState([]); // current guess
   const [isCorrect, setIsCorrect] = useState(false);
   const [guessedCharacter, setGuessedCharacter] = useState({}); //empty character object
@@ -51,22 +51,71 @@ const useGeekle = (solution) => {
     //fetch guessed character from API
     fetchCharacter();
     console.log(guessedCharacter);
+    const charMap = new Map();
+    charMap.set('gender', 'grey');
+    charMap.set('genre', 'grey');
+    charMap.set('role', 'grey');
+    charMap.set('platform', 'grey');
+    charMap.set('show', 'grey');
+    charMap.set('year', 'grey');
 
     //set gender color
 
+    //same gender : set green
+    if(guessedCharacter.gender === solution.gender) {
+      charMap.set('gender', 'green');
+    }
+
     //set show color
+    
+    //same show : set green
+    if(guessedCharacter.show === solution.show) {
+      charMap.set('show', 'green');
+    }
 
     //set genre color
 
+    //same genre : set green
+    if(guessedCharacter.genre === solution.genre) {
+      charMap.set('genre', 'green');
+    }
+
     //set platform color
+
+    //same platform : set green
+    if(guessedCharacter.platform === solution.platform) {
+      charMap.set('platform', 'green');
+    }
 
     //set role color
 
+    //same show : set green
+    if(guessedCharacter.role === solution.role) {
+      charMap.set('role', 'green');
+    }
+
     //set year color
 
-    //setGuessedCharacteristicColors();
+    //same year : set green
+    if(guessedCharacter.year === solution.year) {
+      charMap.set('year', 'green');
+    } else if(Math.floor(guessedCharacter.year / 10) === Math.floor(solution.year / 10)) {
+      charMap.set('year', 'yellow');
+    }
 
     console.log(guessedCharacteristicColors);
+    setGuessedCharacteristicColors((charMap) => {
+      let newGuessedCharacteristicColors = {
+        "name": charMap.get("name"),
+        "gender": charMap.get("gender"),
+        "show": charMap.get("show"),
+        "genre": charMap.get("genre"),
+        "platform": charMap.get("platform"),
+        "role": charMap.get("role"),
+        "year": charMap.get("year"),
+      };
+      return newGuessedCharacteristicColors;
+    });
   };
 
   //add new guesses to the game state
@@ -95,6 +144,10 @@ const useGeekle = (solution) => {
   //if user presses enter, add the new guess
   const handleKeyUp = ({key}) => {
     if(key === 'Enter'){
+      if (turn > 8) {
+        console.log('you used all your guesses!')
+        return
+      }
       if (history.includes(currentGuess)) {
         console.log('you already tried that word.');
         return;
